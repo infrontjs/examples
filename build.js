67417dfc8b431476aec3e17499f0170fe9bdb24d
benny00100 <info@bennyschuetz.com>
2023-02-21 15:17:57 +0100

    REWRITE of build and fix of deployment

---
+++ build.js
@@ -0,0 +1,17 @@
+const fs = require( "fs" );
+const fse = require('fs-extra')
+
+try
+{
+    fs.statSync( './dist' );
+    fs.rmSync( './dist', { recursive: true } );
+}
+catch( e )
+{
+    // Fail silently...
+}
+
+
+fs.mkdirSync( './dist' );
+fse.copySync( 'www', './dist' );
+fse.copySync( 'examples', './dist' );
